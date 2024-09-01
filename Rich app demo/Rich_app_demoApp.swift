3979886cb2a616f7fb616b2f63bb7d7e1420170b
Gozde <gozde.a.997@gmail.com>
2024-09-01 17:18:36 +0300

    Initial Commit

---
+++ Rich app demo/Rich_app_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rich_app_demoApp.swift
+//  Rich app demo
+//
+//  Created by Gözde Aydin on 1.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rich_app_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
